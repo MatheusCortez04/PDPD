422326ccb3bb2e7d209c9e88279515114f39b49c
Matheus Cortez <matheus.cortez@live.com>
2025-10-19 11:05:05 -0300

    feat: add filter in ppi csv file

--- src/PDPD.R
+++ src/PDPD.R
@@ -7,3 +7,5 @@ print("Bibliotecas carregadas com sucesso.")
 print("Realizando Leitura da PPI")
 
 ppi_df  = read.csv(here("Data","PPI_gysi.csv"), sep=",")
+ppi_only_proteins_df = select(ppi_df,proteinA_entrezid,proteinB_entrezid)
+write.csv(ppi_only_proteins_df,"Data/PPI_ONLY_PROTEINS.CSV")
