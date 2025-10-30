c586501e93106bf016fbefc620841b28d07f4ea7
Matheus Cortez <matheus.cortez@live.com>
2025-10-30 14:58:30 -0300

    refactor: refactored pdpd file

--- src/PDPD.R
+++ src/PDPD.R
@@ -1,45 +1,27 @@
+
 library(utils)
 library(here)
-library(dplyr)
-
+library(tidyverse)
 source(here("src","Utils","graph.R"))
 
 source(here("src","Utils","kernels.R"))
-source(here("src","Utils","utils.R"))
+
+source(here("src","Utils","drug.R"))
 main= function(){
+
     cat("\nLibraries loaded successfully.\n")
-    while(TRUE){
-        clear_console()
-        cat("--- R Kernel Generation Menu ---\n\n")
-        cat("Select a kernel to generate:\n")
-        cat(" [1] Diffusion Kernel\n")
-        cat(" [2] P-Step Kernel\n")
-        cat(" [3] Regularised Laplacian Kernel\n")
-        cat(" [4] Commute Time Kernel\n")
-        cat(" [5] Inverse Cosine Kernel\n")
-        cat("\n [q] Quit Application\n\n")
-    input =  readline(prompt = "Enter your choice: ")
-
-    is_valid_input =validate_input(input)
-    if(!is_valid_input){
-        cat("\n[Error] Invalid option. Please try again.\n")
-        Sys.sleep(1.5)
-        next
-    }
-    if( toupper(trimws(input))=="Q"){
-        break
-    }
-    ppi_df  = read.csv(here("src","Data","PPI_gysi.csv"), sep=",")
-    cat("PPI reading complete.\n")
-    cat("Creating dataframe with only proteins\n")
-    ppi_only_proteins_df =  ppi_df %>% select(proteinA_entrezid,proteinB_entrezid)
-    ppi_graph = generate_graph_from_dataframe(ppi_only_proteins_df)
-    kernel_function_mapper[[input]](ppi_graph)
-    
-    }
-
-    q(save="no")
-    }
+        while(TRUE){
+            cat("--- Main Menu ---\n\n")
+            cat(" [1] Generate and Save Kernel\n")
+            cat(" [2] Drug-Disease Score\n")
+            cat(" [q] Exit\n\n")
+            input =  readline(prompt = "Enter your choice: ")
+            if (toupper(trimws(input)) == "Q") break
+            if (input == "1") generate_kernel_menu()
+            if (input == "2") scoring_drug_disease_menu()
+        }
+    q(save="no") 
+}
 
 
 
