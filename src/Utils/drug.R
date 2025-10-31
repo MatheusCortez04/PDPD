d05f189cc27020d67c04d46947f75eafb39495e1
Matheus Cortez <matheus.cortez@live.com>
2025-10-31 20:04:56 -0300

    refactor generate_ordered_drug_protein_matrix

--- src/Utils/drug.R
+++ src/Utils/drug.R
@@ -4,8 +4,8 @@ source(here("src","Utils","utils.R"))
 generate_ordered_drug_protein_matrix = function(drug_target_df,protein_nodes,drug_nodes){
   cat("\n--- Generating Drug Protein Matrix ---\n")
     output_path="drug_gene"
-    path_csv = here("src","Data",paste0(output_path, ".csv"))
-    path_rdata = here("src","Data",paste0(output_path, ".Rdata"))
+    path_csv = here("src","Data","Drug",paste0(output_path, ".csv"))
+    path_rdata = here("src","Data","Drug","RData",paste0(output_path, ".Rdata"))
 
     matrix_drug_protein = drug_target_df %>% 
     filter( entrez_id %in% protein_nodes) %>%
@@ -26,14 +26,14 @@ generate_ordered_drug_protein_matrix = function(drug_target_df,protein_nodes,dru
 
     write.csv(matrix_drug_protein, file =path_csv,row.names=FALSE)
     cat("Csv file  saved to:", path_csv, "\n")
-
+    Sys.sleep(1)
     matrix_drug_protein_numeric <- as.matrix(matrix_drug_protein[ , -1])
     rownames(matrix_drug_protein_numeric) <- matrix_drug_protein$drugbank_id
     storage.mode(matrix_drug_protein_numeric) <- "numeric"
 
     save(matrix_drug_protein_numeric, file = path_rdata)
     cat("R object 'drug_target_df' saved to:", path_rdata, "\n")
-
+    Sys.sleep(1)
 }
 
 
@@ -54,7 +54,7 @@ scoring_drug_disease_menu = function(){
 
 drug_function_mapper <- list(
     '1' = function() {
-        cat("\n--- Generating Drug X Protein Matrix  ---\n")
+        cat("\n--- Generating and  Drug X Protein Matrix  ---\n")
         drug_target_df  = read.csv(here("src","Data","drug_targets_DrugBank_Gysi.csv"), sep=",")
         cat("Drug Target file reading complete.\n")
         ppi_df  = read.csv(here("src","Data","PPI_gysi.csv"), sep=",")
@@ -62,5 +62,21 @@ drug_function_mapper <- list(
         protein_nodes = get_ppi_nodes(ppi_df)
         drug_nodes= get_drug_nodes(drug_target_df)
         generate_ordered_drug_protein_matrix(drug_target_df,protein_nodes,drug_nodes)
+    },
+    '2' = function(){
+      cat("\n--- Generating and save Diseases Vector  ---\n")
+       bipolar_gene= get_bipolar_disorder_genes()
+
+            write.csv(result,"bipolar_gene.csv",)
     }
 )
+
+
+
+        #  drug_protein = load_rdata(here("src","Data","drug_gene.Rdata"))
+        #  kernel  = load_rdata(here("src","Data","Kernels","RData","diffusion_kernel.Rdata"))
+
+        # result <- drug_protein %*% kernel
+
+
+        # write.csv(result,"teste.csv")
