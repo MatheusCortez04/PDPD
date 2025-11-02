a5a3156ead21d8cf333455a385dc9e2b7a3a7523
Matheus Cortez <matheus.cortez@live.com>
2025-11-02 02:33:56 -0300

    refactor menu text

--- src/PDPD.R
+++ src/PDPD.R
@@ -10,14 +10,15 @@ main= function(){
     cat("\nLibraries loaded successfully.\n")
         while(TRUE){
             cat("--- Main Menu ---\n\n")
-            cat(" [1] Generate and Save Kernel\n")
-            cat(" [2] Drug-Disease Score\n")
+            cat(" [1] Kernel Menu\n")
+            cat(" [2] Drug-Disease Menu\n")
             cat(" [Q] Exit\n\n")
             input =  readline(prompt = "Enter your choice: ")
             if (toupper(trimws(input)) == "Q") break
             if (input == "1") generate_kernel_menu()
             if (input == "2") scoring_drug_disease_menu()
         }
+
     q(save="no") 
 }
 
