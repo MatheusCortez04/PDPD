dce69945f6731a3a597acab6eaacad3bfd51cc76
Matheus Cortez <matheus.cortez@live.com>
2025-12-18 15:13:07 -0300

    removed functions in pdpd file

--- src/PDPD.R
+++ src/PDPD.R
@@ -18,10 +18,6 @@ main= function(){
             if (input == "1") generate_kernel_menu()
             if (input == "2") scoring_drug_disease_menu()
         } 
-    # kernel = load_rdata(here("src","Data","Kernels","RData","diffusion_kernel.Rdata"))
-    # drug_protein_matrix = load_rdata(here("src","Data","Drug","RData","drug_target_matrix.Rdata"))
-    # disease_info = list(name = "MDD", vector = build_protein_mdd_df())
-    # process_scores_for_disease(kernel,disease_info,drug_protein_matrix,"diffusion_kernel")
 
     q(save="no") 
 }
