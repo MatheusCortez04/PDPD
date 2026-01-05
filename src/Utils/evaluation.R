ad20956c9e06913d18582096d02daed149b64b19
Matheus Cortez <matheus.cortez@live.com>
2026-01-05 18:13:27 -0300

    refactored recall mdd

--- src/Utils/evaluation.R
+++ src/Utils/evaluation.R
@@ -38,63 +38,61 @@ evaluation_function_mapper = list(
 
 
 generate_recall_k_MDD = function(){
-    prediction_mdd_dir = here("src", "Evaluation","MDD")
-    prediction_mdd_file_name ="prediction_mdd.csv"
-    pred_mdd_rank_file_path = here(prediction_mdd_dir,prediction_mdd_file_name)
-        if (!file.exists(pred_mdd_rank_file_path)) {
-            cat("[WARN] Drug Score :", pred_mdd_rank_file_path, "\n")
-            next()
-        }
-    pred_mdd_rank = read.csv(pred_mdd_rank_file_path)
-    valid_true <- pred_mdd_rank %>%
-    filter(mdd_repodb_validated == "Validated by repODB") %>%
-    pull(drugbank_id)
+    output_dir = here("src", "Evaluation","MDD")
+    prediction_mdd_file_path = here(output_dir,"prediction_mdd.csv")
 
-    pred_mdd_rank = pred_mdd_rank %>%
-        select(drugbank_id,Mean_Rank)
+    if(!file.exists(prediction_mdd_file_path)){
+        cat("[WARN] Processed data not found. Running preparation first...\n")
+        prediction_data = created_prediction_mdd_data()
+    }
+    else{
+        prediction_data = read.csv(prediction_mdd_file_path) 
+    }
 
+    total_hits_count = sum(prediction_data$validation_label)
+    if (total_hits_count == 0) {
+        cat("[WARN] No hits found for recall calculation.\n")
+        return(NULL)
+    }
+    recall_values = cumsum(prediction_data$validation_label) / total_hits_count
 
-    # Atribui valor 1 a toda droga prevista contida no vetor de drogas validas 
-    valid_prediction = ifelse(pred_mdd_rank$drugbank_id %in% valid_true, 1, 0)
+    # #cumSum realiza a soma cumulariva e divide pelo total de verdadeiros positivos
+    # # recall = VP/VP+FN(neste caso nao tem FN a nao ser que seja inserido um valor de corte no rank)
 
-    #cumSum realiza a soma cumulariva e divide pelo total de verdadeiros positivos
-    # recall = VP/VP+FN(neste caso nao tem FN a nao ser que seja inserido um valor de corte no rank)
-    recall_values = cumsum(valid_prediction)/length(valid_true)
+    recall_df = data.frame(
+        K = 1:nrow(prediction_data),
+        Recall = recall_values
+    )
 
-    recall_df= data.frame(K = 1:nrow(pred_mdd_rank),
-                        Recall = recall_values)
-
-        recall_df$highlight <- ifelse(recall_df$K %% 50 == 0, TRUE, FALSE)
+    recall_df$highlight <- ifelse(recall_df$K %% 50 == 0, TRUE, FALSE)
         g = ggplot(recall_df, aes(x = K, y = Recall)) +
         geom_line(size = 1) +
         geom_point(
-        data = subset(recall_df, highlight == TRUE),
-        size = 3,
-        color = "red"
-        ) +
+            data = subset(recall_df, highlight == TRUE),
+            size = 3,
+            color = "red") +
     geom_text(
-    data = subset(recall_df, highlight == TRUE),
-    aes(label = paste0("(", K, ", ", round(Recall, 3), ")")),
-    vjust = -0.7,
-    size = 3,
-    check_overlap=TRUE
+        data = subset(recall_df, highlight == TRUE),
+        aes(label = paste0("(", K, ", ", round(Recall, 3), ")")),
+        vjust = -0.7,
+        size = 3,
+        check_overlap=TRUE
     )+
-        labs(
-            title = "Recall@K para MDD",
-            x = "K (Top-K)",
-            y = "Recall"
-        ) +
-        theme_minimal(base_size = 14)
+    labs(
+        title = "Recall@K para MDD",
+        x = "K (Top-K)",
+        y = "Recall") +
+    theme_minimal(base_size = 14)
     print(g)
 
-    recall_path_dir = here(prediction_mdd_dir,"RECALL")
-    pdf_path <- here(recall_path_dir,"recall_at_k_MDD.pdf")
-    dir.create(recall_path_dir, recursive = TRUE, showWarnings = FALSE)
+    output_recall_dir= here(output_dir,"Recall")
+    dir.create(output_recall_dir, recursive = TRUE, showWarnings = FALSE)
+    pdf_path = here(output_recall_dir,"recall_at_k_MDD.pdf")
     ggsave(pdf_path, plot = g, width = 8, height = 6)
-
-    message(sprintf("[SUCCESS] Recall graph saved at: %s",here(pdf_path)))
+    message(sprintf("[SUCCESS] Recall@K graph saved at: %s",here(output_recall_dir,"recall_at_k_MDD.pdf")))
     Sys.sleep(1.5)
 }
+
 
 generate_roc_curve_mdd = function(){
     output_dir = here("src", "Evaluation","MDD")
@@ -131,24 +129,11 @@ generate_roc_curve_mdd = function(){
 
 created_prediction_mdd_data = function(){
     drug_target_mapping  = load_drug_target_df()
-    mdd_gold_standard_repodb = read_tsv(here("src","Data","REPODB","MDD_REPODB.tsv"), show_col_types = FALSE)    
-    mdd_gold_standard_repodb = mdd_gold_standard_repodb %>%  dplyr::semi_join(drug_target_mapping,by='drugbank_id') %>% 
+    mdd_gold_standard = read_tsv(here("src","Data","REPODB","MDD_REPODB.tsv"), show_col_types = FALSE)    
+    mdd_gold_standard = mdd_gold_standard %>%  dplyr::semi_join(drug_target_mapping,by='drugbank_id') %>% 
       filter(status=="Approved")
  
-    cat(sprintf("[INFO] Total valid drugs in RepoDB (Gold Standard): %d\n",nrow(mdd_gold_standard_repodb)))
-
-    mdd_gold_standard_open_targets = read.csv(here("src","Data","REPODB","openTargets_final_mapeado.csv")) %>%
-      dplyr::semi_join(drug_target_mapping, by='drugbank_id')
-
-    drugbank_id_repodb = unique(mdd_gold_standard_repodb$drugbank_id)
-    drugbank_id_op   = unique(mdd_gold_standard_open_targets$drugbank_id) # Aqui está o segredo
-    
-    merge = unique(c(drugbank_id_op, drugbank_id_repodb))
-
-    cat(sprintf("[INFO] Unique drugs in RepoDB: %d\n", length(drugbank_id_repodb)))    
-    cat(sprintf("[INFO] Unique drugs in OpenTargets: %d\n", length(drugbank_id_op)))  
-    cat(sprintf("[INFO] Total unique drugs in MERGE: %d\n", length(merge)))
-    
+    cat(sprintf("[INFO] Total valid drugs in RepoDB (Gold Standard): %d\n",nrow(mdd_gold_standard)))
     rank_file_path = here("src", "Data", "Drug", "Score", "MDD", "average_kernel_rank.csv")
     
     if (!file.exists(rank_file_path)) {
@@ -157,10 +142,10 @@ created_prediction_mdd_data = function(){
      mdd_prediction = read.csv(rank_file_path)
     processed_predictions = mdd_prediction %>% 
         dplyr::mutate(
-            validation_label =  ifelse(drugbank_id %in% merge, 1, 0),
+            validation_label =  ifelse(drugbank_id %in% mdd_gold_standard$drugbank_id, 1, 0),
             validation_status = factor(validation_label,
                                              levels = c(0, 1),
-                                             labels = c("Not validated", "Validated by Gold Standard"))
+                                             labels = c("Not validated by RepoDB", "Validated by RepoDB"))
             )
 
     validated_hits = processed_predictions %>% filter(validation_label==1)
@@ -176,4 +161,65 @@ created_prediction_mdd_data = function(){
     message(sprintf("[SUCCESS] prediction file  saved at: %s",here(output_dir,"prediction_mdd.csv")))
 
     invisible(processed_predictions)
+}
+generate_roc_to_kernel = function(){
+    diseases = c("MDD","BD")
+    drug_score_dir =here("src","Data","Drug","Score")
+     
+    
+    kernel_names = c(
+        "diffusion_kernel","pstep_kernel","regularised_laplacian_kernel",
+        "commute_time_kernel","inverse_cosine_kernel"
+    )
+    for(disease in diseases){
+        for(kernel in kernel_names){
+            gold_standard_file_path = here("src","Data","REPODB",paste0(disease,"_REPODB.tsv"))
+            if (!file.exists(gold_standard_file_path)) {
+                cat("[WARN] Drug Score to kernel:",kernel,"and disease:",disease,"\n")
+                next()
+            }
+            gold_standard = read_tsv(gold_standard_file_path, show_col_types = FALSE)   
+            drug_target_mapping  = load_drug_target_df()
+
+            gold_standard = gold_standard %>% filter(status=="Approved") %>%
+                dplyr::semi_join(drug_target_mapping,by='drugbank_id')
+                input_file_path =here(drug_score_dir,disease,paste0(kernel,".csv")) 
+                if (!file.exists(input_file_path)) {
+                    cat("[WARN] not found gold standard file to disease:",disease,"\n")
+                    next()
+                }
+                drug_score_kernel = read.csv(input_file_path)
+
+                kernel_predictions = drug_score_kernel %>% 
+                    dplyr::mutate(
+                        validation_label =  ifelse(drugbank_id %in% gold_standard$drugbank_id, 1, 0),
+                        validation_status = factor(validation_label,
+                                             levels = c(0, 1),
+                                             labels = c("Not validated by RepoDB", "Validated by RepoDB"))
+            )
+
+
+                validated_hits = kernel_predictions %>% filter(validation_label==1)
+                cat(sprintf("[INFO] Total predicted hits validated: %d\n",nrow(validated_hits)))
+                if (nrow(validated_hits) <= 0) {
+                    cat("[WARN] ROC curve unavailable to disease: ",disease,"\nNo validated drugs were predicted in the ranking.\n")
+                    Sys.sleep(1.5)
+                    return(NULL)
+                }
+                output_roc_dir = here("src","Evaluation",disease,"ROC","KERNEL")
+                output_graph_file_name = paste0(kernel,"_roc_curve.pdf")
+                dir.create(output_roc_dir, recursive = TRUE, showWarnings = FALSE)
+
+                grDevices::pdf(here(output_roc_dir,output_graph_file_name), width = 6, height = 6)
+                roc_results = reportROC::reportROC(
+                    gold = kernel_predictions$validation_label,
+                    predictor =kernel_predictions$drug_score,
+                    plot = TRUE)
+
+                
+                grDevices::dev.off()
+                message(sprintf("[SUCCESS] ROC curve saved at: %s",here(output_roc_dir, output_graph_file_name)))
+                Sys.sleep(1.5)
+        }
+    }
 }
