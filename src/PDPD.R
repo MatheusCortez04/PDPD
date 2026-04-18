5e0fd7cd8a258926a9ff429f84e48ae3af778f90
Matheus Cortez <matheus.cortez@live.com>
2026-04-18 17:37:20 -0300

    create generate_drug_rank

--- src/PDPD.R
+++ src/PDPD.R
@@ -8,36 +8,20 @@ source(here("src","Utils","drug.R"))
 source(here("src","Utils","evaluation.R"))
 main= function(){
 
-    # cat("\nLibraries loaded successfully.\n")
-    #     while(TRUE){
-    #         cat("--- Main Menu ---\n\n")
-    #         cat(" [1] Kernel Menu\n")
-    #         cat(" [2] Drug Menu\n")
-    #         cat(" [3] Evaluation Menu\n")
-    #         cat(" [Q] Exit\n\n")
-    #         input =  readline(prompt = "Enter your choice: ")
-    #         if (toupper(trimws(input)) == "Q") break
-    #         if (input == "1") generate_kernel_menu()
-    #         if (input == "2") scoring_drug_disease_menu()
-    #         if (input == "3") evaluation_menu()
-    #     }
-    # mdd_genes_vector =get_mdd_genes()
-#      calculate_drug_score()
-#      generate_drug_rank()
-# created_prediction_mdd_data()
+    cat("\nLibraries loaded successfully.\n")
+        while(TRUE){
+            cat("--- Main Menu ---\n\n")
+            cat(" [1] Kernel Menu\n")
+            cat(" [2] Drug Menu\n")
+            cat(" [3] Evaluation Menu\n")
+            cat(" [Q] Exit\n\n")
+            input =  readline(prompt = "Enter your choice: ")
+            if (toupper(trimws(input)) == "Q") break
+            if (input == "1") generate_kernel_menu()
+            if (input == "2") scoring_drug_disease_menu()
+            if (input == "3") evaluation_menu()
+        }
 
-#      generate_roc_curve_mdd()
-calculate_drug_score()
-generate_drug_rank("BD")
-generate_drug_rank("MDD")
-created_prediction_bipolar_data()
-created_prediction_mdd_data()
-generate_roc_curve_bipolar()
-generate_roc_curve_mdd()
-    # targets_validos <- intersect(alvos_teste, rownames(kernel))
-    # disease_genes_validos <- intersect(mdd_genes_vector$entrez_id, rownames(kernel))
-    # score_droga_teste = kernel[targets_validos,disease_genes_validos]
-   
 
     q(save="no") 
 }
--- src/Utils/drug.R
+++ src/Utils/drug.R
@@ -19,8 +19,7 @@ scoring_drug_disease_menu = function(){
 drug_function_mapper = list(
     '1' = function() {
         calculate_drug_score()
-        generate_drug_rank("MDD")
-        generate_drug_rank("BD")
+        generate_drug_rank()
     }
 )
 
@@ -45,11 +44,12 @@ get_drug_targets = function(drugbank_id,ppi_gene_nodes,drug_target_df) {
         pull(entrez_id)
 
     drug_target_proteins = intersect(drug_target_proteins, ppi_gene_nodes)
-    cat(sprintf("[INFO] For DrugBank ID %s, found %d unique targets.\n",drugbank_id,length(drug_target_proteins)))
     invisible(drug_target_proteins)
 }
 
 calculate_drug_score = function(){
+    
+    cat(sprintf("\n[INFO] Starting drug scoring pipeline...\n"))
     kernel_names = c(
         "diffusion_kernel","pstep_kernel","regularised_laplacian_kernel",
         "commute_time_kernel","inverse_cosine_kernel"
@@ -62,20 +62,48 @@ calculate_drug_score = function(){
     drugbank_ids = get_drugbank_ids()
     ppi_gene_nodes = get_ppi_nodes()
     drug_target_df=read_drug_targets()
+
+    cat(sprintf("[INFO] Total drugs: %d | PPI nodes: %d\n",
+        length(drugbank_ids), length(ppi_gene_nodes)))
+
     drug_targets = purrr::map(drugbank_ids, get_drug_targets, ppi_gene_nodes,drug_target_df)
     names(drug_targets) = drugbank_ids
 
     for(kernel_name in kernel_names){
-        kernel_path <- here("src","Data","Kernels","RData", paste0(kernel_name, ".Rdata"))
+        
+        cat(sprintf("\n[INFO] Processing kernel: %s\n", kernel_name))
+
+        kernel_path = here("src","Data","Kernels","RData", paste0(kernel_name, ".Rdata"))
+
         if (!file.exists(kernel_path)) {
-            cat("[WARN]Kernel:",kernel_name, "file not found.\n")
+            cat("[WARN]Kernel:",kernel_name, "file not found. Skipping....\n")
             next()
         }
-        kernel_matrix <- load_rdata(kernel_path)
+
+        kernel_matrix = load_rdata(kernel_path)
         for(disease in names(disease_data)){
-            disease_genes <- disease_data[[disease]]$genes
+
+            score_base_dir = here("src", "Data", "Drug", "Score", disease)
+            create_dir(score_base_dir)
+
+            disease_genes = disease_data[[disease]]$genes
+            
+            cat(sprintf("[INFO] Kernel: %s | Disease: %s | Genes: %d\n",kernel_name, disease, length(disease_genes)))
+
             scores = purrr::map_dbl(drug_targets,compute_drug_score,disease_genes,kernel_matrix)
-             print(head(scores))
+            valid_scores = sum(!is.na(scores))
+            na_scores = sum(is.na(scores))
+
+            cat(sprintf("[INFO] Kernel: %s | Disease: %s | Valid scores: %d | NA scores: %d\n",kernel_name, disease, valid_scores, na_scores))
+
+
+            result_df = data.frame(
+            drugbank_id = names(scores),
+            score = as.numeric(scores))
+            
+            output_file_name = here(score_base_dir, paste0(kernel_name, ".csv"))
+            write.csv(result_df,file=output_file_name,row.names=FALSE)
+            cat(sprintf("✔ [DONE] Kernel: %s | Disease: %s | Saved: %s\n",kernel_name, disease, output_file_name))
         }
     }
 
@@ -91,5 +119,59 @@ compute_drug_score = function(targets,disease_genes,kernel_matrix) {
   if (length(targets) <= 0) {
     return(NA_real_)
   }
-  mean(kernel_matrix[targets, disease_genes, drop = FALSE], na.rm = TRUE)
+  max(kernel_matrix[targets, disease_genes, drop = FALSE], na.rm = TRUE)
+}
+
+generate_drug_rank = function() {
+    diseases = c("MDD", "BD")
+    
+    cat("\n[INFO] Starting drug ranking aggregation...\n")
+
+    for(disease in diseases) {
+        cat(paste("\n[INFO] Building Average Rank to Disease:", disease, "\n"))
+        score_base_dir = here::here("src", "Data", "Drug", "Score", disease)
+        
+        pstep_df = read.csv(here::here(score_base_dir, "pstep_kernel.csv")) %>%
+            dplyr::mutate(pstep_kernel = rank(dplyr::desc(score), ties.method = "average")) %>%
+            dplyr::select(drugbank_id, pstep_kernel)
+
+        reg_lap_df = read.csv(here::here(score_base_dir, "regularised_laplacian_kernel.csv")) %>%
+            dplyr::mutate(regularised_laplacian_kernel = rank(dplyr::desc(score), ties.method = "average")) %>%
+            dplyr::select(drugbank_id, regularised_laplacian_kernel)
+        inv_cos_df = read.csv(here::here(score_base_dir, "inverse_cosine_kernel.csv")) %>%
+           dplyr::mutate(inverse_cosine_kernel = rank(dplyr::desc(score), ties.method = "average"))  %>%
+            dplyr::select(drugbank_id, inverse_cosine_kernel)
+
+        commute_df = read.csv(here::here(score_base_dir, "commute_time_kernel.csv")) %>%
+             dplyr::mutate(commute_time_kernel = rank(dplyr::desc(score), ties.method = "average"))  %>%
+            dplyr::select(drugbank_id, commute_time_kernel)
+
+        diffusion_df = read.csv(here::here(score_base_dir, "diffusion_kernel.csv")) %>%
+             dplyr::mutate(diffusion_kernel = rank(dplyr::desc(score), ties.method = "average"))  %>%
+            dplyr::select(drugbank_id, diffusion_kernel)
+
+        merge_df = pstep_df %>%
+            dplyr::left_join(reg_lap_df, by = "drugbank_id") %>%
+            dplyr::left_join(commute_df, by = "drugbank_id") %>%
+            dplyr::left_join(inv_cos_df, by = "drugbank_id") %>%
+            dplyr::left_join(diffusion_df, by = "drugbank_id") 
+        
+        cat(sprintf(
+            "[INFO] Merge complete | Rows: %d | Columns: %d\n",nrow(merge_df), ncol(merge_df)))
+
+        final_df <- merge_df %>%
+            dplyr::mutate(
+                average_rank = rowMeans(
+                    dplyr::select(., dplyr::ends_with("_kernel")), 
+                    na.rm = TRUE
+                )
+            ) %>%
+            dplyr::arrange(average_rank) %>%
+            dplyr::select(drugbank_id,dplyr::ends_with("_kernel"),average_rank)
+        output_file_name=here(score_base_dir,paste0("average_rank_", disease, ".csv"))        
+        write.csv(final_df, file=output_file_name , row.names = FALSE)
+
+        cat(sprintf("✔ [DONE] Disease: %s | Saved: %s\n", disease, output_file_name))
+    }
+
 }
