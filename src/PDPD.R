aa4eb7fc2b9ea577e17f161f9b1ad09a56919c43
Matheus Cortez <matheus.cortez@live.com>
2026-05-18 21:48:46 -0300

    create graph file

--- src/PDPD.R
+++ src/PDPD.R
@@ -13,12 +13,14 @@ main= function(){
             cat(" [1] Kernel Menu\n")
             cat(" [2] Drug Menu\n")
             cat(" [3] Evaluation Menu\n")
+            cat(" [4] Graph Menu\n")
             cat(" [Q] Exit\n\n")
             input =  readline(prompt = "Enter your choice: ")
             if (toupper(trimws(input)) == "Q") break
             if (input == "1") generate_kernel_menu()
             if (input == "2") scoring_drug_disease_menu()
             if (input == "3") evaluation_menu()
+            if (input == "4") graph_menu()
         }
     q(save="no") 
 }
