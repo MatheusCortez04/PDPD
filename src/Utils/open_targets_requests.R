f19d1dc9ecd182b3c7a2e67d3a6ff1e7e9c2f3d2
Matheus Cortez <matheus.cortez@live.com>
2026-05-24 11:45:33 -0300

    create get_drug_candidates in open targets requests file

--- src/Utils/open_targets_requests.R
+++ src/Utils/open_targets_requests.R
@@ -1,5 +1,6 @@
 library(httr2)
 library(dplyr)
+library(purrr)
 
 get_open_target_config = function(){
     return (
@@ -13,25 +14,33 @@ get_open_target_config = function(){
                                 name
                             }
                         }
-                    }',
-                clinical_report='query RecordDetailQuery($clinicalReportId: String!) {
-                    clinicalReport(clinicalReportId: $clinicalReportId) {
-                        id
-                        title
-                        clinicalStage
-                        source
-                        url
+                }',
+                drug_candidates='query drugsQuery($efoId: String!) {
+                    disease(efoId: $efoId) {
+                        name
+                            drugAndClinicalCandidates {
+                            count
+                            rows {
+                                id
+                                maxClinicalStage
+                                drug {
+                                    id
+                                    name
+                                }
+                                clinicalReports {
+                                    id
+                                }
+                            }
+                        }
                     }
-                }'    
-            
-            )
-            
+                }'
+            ),
+            disease_ids = list("MDD" = "MONDO_0002009", "BD" = "MONDO_0004985")
     ))
 }
 
 
 get_drug_info_from_dbid = function (drugbank_id) {
-
   message(sprintf("[REQUEST] get_drug_info_from_dbid() | drugbank_id=%s", drugbank_id))
   opentarget_config = get_open_target_config()
 
@@ -67,3 +76,72 @@ get_drug_info_from_dbid = function (drugbank_id) {
   )
   invisible(drug)
 }
+
+get_drug_candidates = function (disease = c("MDD", "BD")) {
+  disease = match.arg(disease)
+
+  opentarget_config = get_open_target_config()
+  clinical_stage_map = c(
+    "PRECLINICAL" = 0,
+    "PHASE_1" = 1,
+    "PHASE_1_2" = 1.5,
+    "PHASE_2" = 2,
+    "PHASE_2_3" = 2.5,
+    "PHASE_3" = 3,
+    "PHASE_4" = 4,
+    "APPROVED" = 5,
+    "WITHDRAWN" = -1
+  )
+  
+  disease_ids = opentarget_config$disease_ids
+  endpoint = opentarget_config$url
+
+  graphql_query = opentarget_config$queries$drug_candidates
+
+  response = httr2::request(endpoint) %>%
+    httr2::req_body_json(
+      list(
+        query = graphql_query,
+        variables = list(efoId = disease_ids[[disease]])
+      )
+    ) %>%
+    httr2::req_perform()
+
+  data = response %>% resp_body_json()
+  rows = purrr::pluck(
+    data,
+    "data",
+    "disease",
+    "drugAndClinicalCandidates",
+    "rows"
+  )
+  if (is.null(rows) || length(rows) == 0) {
+    return (NA)
+  }
+
+  parsed_data = purrr::map_dfr(rows, function (indication_drug) {
+      first_report_id = ifelse(length(indication_drug$clinicalReports) > 0,
+        indication_drug$clinicalReports[[1]] $id, NA_character_)
+      tibble::tibble(
+        drug_id = indication_drug$drug$id %||% NA_character_,
+        clinical_report_id = first_report_id,
+        max_clinical_stage_open_targets = factor(
+          indication_drug$maxClinicalStage,
+          levels = c(
+            "PRECLINICAL",
+            "PHASE_1",
+            "PHASE_1_2",
+            "PHASE_2",
+            "PHASE_2_3",
+            "PHASE_3",
+            "PHASE_4",
+            "APPROVAL"
+          )
+        ),
+      )
+    }) %>%
+    dplyr::distinct() %>%
+    dplyr::arrange(desc(max_clinical_stage_open_targets))
+  return (parsed_data)
+}
+
