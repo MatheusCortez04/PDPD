b4ceee26726804c3dd52ce4bee9fc43072c29fa0
Matheus Cortez <matheus.cortez@live.com>
2026-06-26 13:34:01 -0300

    add safe function

--- src/Utils/open_targets_requests.R
+++ src/Utils/open_targets_requests.R
@@ -180,3 +180,6 @@ get_clinical_report_data= function(clinicalReportId){
     ))
 }
 
+get_clinical_report_data_safe = purrr::possibly(get_clinical_report_data, otherwise = list(
+    source = NA, url = NA, evidence_summary = "ERRO_NA_REQ"
+))
